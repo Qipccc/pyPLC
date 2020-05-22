fd61f1a61d1495689bccac19053b04dacd89ed63
qipccc <qipccc@qq.com>
2020-05-22 15:26:10 +0800

    完成基本需求日志文件超过30个自动清理

--- src/PLC_TCP2GH_UDP.py
+++ src/PLC_TCP2GH_UDP.py
@@ -8,8 +8,9 @@ import struct
 import argparse
 import _thread
 
-from Utils import create_logger, create_TempFolder
+from Utils import create_logger, create_TempFolder,readText2Dic
 
+ERROR_DICT ={}
 def get_time():
     end_time = time.clock()
     print("运行时长： %.2f 秒"%(end_time-start_time), end="\r")
@@ -21,9 +22,9 @@ class GhClient(object):
         self.udpserver = socket.socket(socket.AF_INET,socket.SOCK_DGRAM)
     
     def broadcast(self, msg_str):
-        LOG.debug("向grasshopper 广播数据: %s"%msg_str)
+        LOG.info("向grasshopper 广播数据: %s"%msg_str)
         self.udpserver.sendto(msg_str.encode("utf-8"), self.udpaddr)
-        LOG.debug("广播结束")
+        LOG.info("广播结束")
 
 class PlcClient(object):
     def __init__(self,addr, port, timeout=3):
@@ -45,7 +46,7 @@ class PlcClient(object):
             robot_num: int ,0,1,2... which robot
         """
         res = [-999] * 8 
-        LOG.debug("接收得到 机械臂 %d 数据......"%robot_num)
+        LOG.info("接收得到 机械臂 %d 数据......"%robot_num)
         for i in range(robot_num*10,robot_num*10+8):
             info_data = info_str[(i+1)*10:(i+2)*10].rstrip().lstrip() 
             if info_data:
@@ -55,8 +56,7 @@ class PlcClient(object):
     def sendRes(self,bufsize=200, ghclient = None):
         count = 10 
         flag = -1
-        LOG.info("监听程序....")
-        LOG.info("机械臂正在运动至指定位置....")
+        # LOG.info("机械臂正在运动至指定位置....")
         while count > 0:  
             packet = self.s.recv(bufsize)
             info = struct.unpack("!200s",packet)[0]
@@ -64,17 +64,18 @@ class PlcClient(object):
             head1 = info_str[:10].rstrip().lstrip() # 获得前10个字符数据并进行判断
             head2 = info_str[100:110].rstrip().lstrip()
             if head1 == "errorstart":
-                error_info = info_str[10:].rstrip().lstrip()
+                error_index = info_str[10:].rstrip().lstrip()
+                error_info = ERROR_DICT[int(error_index)]
                 # todo 查找alarm_information
-                raise Exception("%s error: %s"%(time.strftime("%Y-%m-%d %H:%M:%S", time.localtime()) , error_info))   # 用于后面捕获异常
+                raise Exception("Error: %s"%(error_info))   # 用于后面捕获异常
             if head1 == "start1":
                 res1 = self._decodeLaserDate(0, info_str)
                 flag += 1
-                LOG.debug("res1: %s"%res1)    
+                LOG.info("res: %s"%res1)    
             if head2 == "start2":
                 res2 = self._decodeLaserDate(1, info_str)
                 flag += 1
-                LOG.debug("res2: %s"%res2)
+                LOG.info("res: %s"%res2)
             if ghclient and flag >= 0:
                 msg = self.encodeUdpMsg([res1,res2])
                 ghclient.broadcast(msg)
@@ -87,11 +88,7 @@ class PlcClient(object):
             msg += ";"
         return msg
 
-def ghUDP(msg):
-    udpsever=socket.socket(socket.AF_INET,socket.SOCK_DGRAM)
-    server_address = (args['gh_addr'], args['gh_port'])  # 接收方 服务器的ip地址和端口号
-    print(msg)
-    udpsever.sendto(msg.encode("utf-8"), server_address) #将msg内容发送给指定接收方
+
 
 if __name__=="__main__":
     parser = argparse.ArgumentParser()
@@ -100,6 +97,7 @@ if __name__=="__main__":
     parser.add_argument('--plc-addr','-pa', type=str, default="192.168.100.1")
     parser.add_argument('--plc-port','-pp', type=int, default=3000)
     global args
+
     #=========== 变量初始化 ==========================================
     args = vars(parser.parse_args())
     start_time = time.clock() # 程序开始计时
@@ -109,11 +107,12 @@ if __name__=="__main__":
     gh_port = args['gh_port']
 
     #=========== 创建日志文件 ==========================================
-    logfilePath = create_TempFolder("RobotsPlc.log")
+    logfilePath = create_TempFolder("RobotsPlc")
     LOG = create_logger("RobotsPlc",logfilePath)
-    LOG.info("上海大界机器人科技有限公司 \n 运行红外测试程序....") 
-    LOG.debug("PLC ip: %s, port: %s"%(plc_addr, plc_port))
-    LOG.debug("Grasshopper ip :%s, port: %s"%(gh_addr, gh_port))
+    LOG.info("\n %s 上海大界机器人科技有限公司 %s \n  运行红外测试程序...... "%("="*15,"="*15)) 
+    LOG.info("PLC ip: %s, port: %s"%(plc_addr, plc_port))
+    LOG.info("Grasshopper ip :%s, port: %s"%(gh_addr, gh_port))
+    LOG.info("监听程序....")
 
     #=========== 多线程计时 =============================================
     _thread.start_new_thread(get_time,())
@@ -122,9 +121,11 @@ if __name__=="__main__":
     #=========== 分别声明 plc 和 grasshopper 的地址和端口对象 ============= 
     client = PlcClient(plc_addr, plc_port)
     ghclient = GhClient(gh_addr, gh_port)
-    #===========
+
+    #=========== 尝试与PLC建立连接 ======================================= 
     try:
         client.connect()
+        ERROR_DICT = readText2Dic("alarm_information.txt")
     except Exception as e:
         LOG.error(e)
 
--- src/Utils.py
+++ src/Utils.py
@@ -2,6 +2,7 @@
 import sys
 import logging
 import os
+import datetime
 
 def create_logger(logger_name, log_file):
     '''
@@ -12,27 +13,57 @@ def create_logger(logger_name, log_file):
     Use   logger = logging.getLogger(logger_name) to obtain logging all
     through out
     '''
-    # 需要添加清理日志的功能（保留日志信息 一周）
+    # Todo需要添加清理日志的功能（保留日志信息 一周）
     logger = logging.getLogger(logger_name)
     # Remove the stdout handler
     logger_handlers = logger.handlers[:]
     for handler in logger_handlers:
         if handler.name == 'std_out':
             logger.removeHandler(handler)
-    logger.setLevel(logging.DEBUG)
+    logger.setLevel(logging.INFO)
+
     file_h = logging.FileHandler(log_file)
-    file_h.setLevel(logging.DEBUG)
+    file_h.setLevel(logging.INFO)
     file_h.set_name('file_handler')
+
     terminal_h = logging.StreamHandler(sys.stdout)
     terminal_h.setLevel(logging.INFO)
     terminal_h.set_name('stdout')
+
     formatter = logging.Formatter('%(asctime)s - %(levelname)s - %(message)s')
     tool_formatter = logging.Formatter(' %(levelname)s - %(message)s')
     file_h.setFormatter(formatter)
     terminal_h.setFormatter(tool_formatter)
+
     logger.addHandler(file_h)
     logger.addHandler(terminal_h)
     return logger
+
+def delExpireLog(path):
+    list_path = os.listdir(path)
+    flag = 0
+    if len(list_path) > 30:
+        for f in enumerate(list_path):
+            #ToDo 自动识别不符合要求的文件，并进行删除，防止文件奔溃
+            date = f.split('.')[0].split('_')[-1]
+            year,month,day = date.split('-')
+            try:
+                date_num = int(year) * 10000 + int(month) * 100 + int(day)
+            except:
+                os.remove(os.path.join(path, f))
+                continue
+            if flag == 0:
+                min_date = date_num
+                del_f = f
+                flag +=1
+            else:
+                if date_num < min_date:
+                    min_date = date_num
+                    del_f = f
+        del_filepath = os.path.join(path, del_f)
+        os.remove(del_filepath)
+    else:
+        pass
 
 def create_TempFolder(filename):
     """
@@ -45,5 +76,21 @@ def create_TempFolder(filename):
     rpLogerFolder = os.path.join(rpFolder,"log")
     if not os.path.exists(rpLogerFolder):
         os.mkdir(rpLogerFolder)
+    current_date = str(datetime.date.today())  # 2020-05-22
+    filename = filename + '_' + current_date + '.log'
+    # Todo 仅保留一个的日志文件，其他可以删除
+    delExpireLog(rpLogerFolder)
     return os.path.join(rpLogerFolder, filename)
-    
+
+def readText2Dic(fname):
+    data_dict = {}
+    try:
+        with open(fname,'r',encoding='utf-8') as f:
+            for line in f:
+                t = line.split(":")
+                key = int(t[0].lstrip().rstrip())
+                value = t[1].split('\n')[0].lstrip().rstrip()
+                data_dict.update({key:value})
+    except Exception as e:
+        raise e
+    return data_dict
